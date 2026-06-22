6004ca562115e7d40e1cfdee3c4e917e63f7d0fa
tosuki <okaabe2006@gmail.com>
2026-06-21 21:33:29 -0300

    fix: Inicializa conexão com o MongoDB antes de inicializar o servidor http

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,12 +2,14 @@ import "dotenv/config";
 import fastify from "./http/server"
 
 import { env } from './config/env';
-
+import { getMongoDatabaseConnector } from "./factory";
 
 const start = async () => {
   try {
     const port = env.PORT;
     const host = '0.0.0.0';
+    
+    await getMongoDatabaseConnector().connect()
     await fastify.listen({ port, host });
     console.log(`Servidor rodando em http://localhost:${port}`);
   } catch (err) {
